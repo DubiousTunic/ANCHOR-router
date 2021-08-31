f744cf3424b7901c5875aed99f19afe0564fcc0e
DubiousTunic <43465053+DubiousTunic@users.noreply.github.com>
2021-08-31 14:40:19 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -3,20 +3,61 @@
 $(document).ready(function(){
 	$(".partial").hide();
 
+	//routes the path on load
+	route(window.location.pathname + window.location.hash + window.location.search)
+
 	$(".anch_link").click(function(e){
-		e.preventDefault()
-		console.log($(this).attr("id"));
-		var anchorClass = getAnchorClass($(this));
-		router(anchorClass)
+		e.preventDefault();
+		route("#" + anchorPath($(this)) + anchorParams($(this)))
 	})
 
 })
 
-function getAnchorClass(href){
+function hidePartial(){
+	$(".partial").hide();
+}
+
+function getLink(origin){
+	var params = origin.split("?")
+	var path = params[0].split("#");
+	return {
+		path : path[1],
+		params : params
+	}
+}
+
+function route(origin){
+	console.log("INITIALIZE ORIGIN " + origin);
+	hidePartial();
+	var link = getLink(origin);			
+	history.pushState(origin, '', origin)
+	router(link.path, link.params)			
+}
+
+window.addEventListener('popstate', function(event){
+	var origin = event.state;
+
+	//route(path);
+	if(origin !== null){	
+		var link = getLink(origin);
+		hidePartial();
+		router(link.path, link.params);		
+	}									
+})
+
+function anchorPath(href){
 	return href.attr("class").split(/\s+/)[1];
 }
 
-function router(anchorClass){
-	$(".partial").hide();
-	$("div." + anchorClass).show();
+function anchorParams(href){
+	return href.attr("rel");
+}
+
+//make back/forward work
+
+//main router fn
+function router(path, params){
+	$("div." + path).show();
+
+	//display path, params in url
 }
